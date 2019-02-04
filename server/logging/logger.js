b2b0f72be2df4f4a7556a55ffa44484794f8d60e
Evans Djangbah <djangbahevans@yahoo.com>
2019-02-04 17:04:12 +0000

    Return 404 for files not found.

--- server/logging/logger.js
+++ server/logging/logger.js
@@ -8,5 +8,5 @@ module.exports = winston.createLogger({
     level: 'info',
     format: winston.format.prettyPrint(),
     transports: new winston.transports.File({ filename: path.join(__dirname, '..', '..', 'logfile.log') }),
-    exceptionHandlers: new winston.transports.File({ filename: path.join(__dirname, '..', '..', 'exceptions.log')})
+    exceptionHandlers: new winston.transports.File({ filename: path.join(__dirname, '..', '..', 'exceptions.log')}),
 });
