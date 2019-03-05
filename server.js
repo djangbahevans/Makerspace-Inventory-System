638806900dfa8839e19e61074e4d961647400b62
Evans Djangbah <djangbahevans@yahoo.com>
2019-03-05 12:39:07 +0000

    Created default user

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-const { ApolloServer, gql } = require('apollo-server-express'); ``
+const {
+    ApolloServer,
+    gql
+} = require('apollo-server-express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -16,19 +19,25 @@ const Mutation = require('./server/resolvers/Mutation');
 const Query = require('./server/resolvers/Query');
 const Requisition = require('./server/resolvers/Requisition');
 const Stock = require('./server/resolvers/Stock');
+const User = require('./server/models/User');
+const createDefaultUser = require('./server/fixtures/createDefaultUser');
 
 
-const PRODUCTION = process.env.NODE_ENV ? true : false;
+const PRODUCTION = process.env.NODE_ENV === 'production'
 
 // Logging
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'express.log'), { flags: 'a' });
-app.use(morgan('combined', { stream: accessLogStream }));
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'express.log'), {
+    flags: 'a'
+});
+app.use(morgan('combined', {
+    stream: accessLogStream
+}));
 
 // Database Connection
 const URI = process.env.URI || "mongodb://mongo/inventory";
 mongoose.connect(URI, {
-    useNewUrlParser: true
-})
+        useNewUrlParser: true
+    })
     .then(() => logger.info('Connected to  MongoDB'))
     .catch(() => {
         logger.info('Could not connect to MongoDB');
@@ -38,35 +47,49 @@ mongoose.connect(URI, {
 // 3rd Party Middleware 
 app.use(helmet())
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
     secret: process.env.SessionSecret || 'keyboard cat',
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({ mongooseConnection: mongoose.connection }),
-    cookie: { secure: true }
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection
+    }),
+    cookie: {
+        secure: true
+    }
 }));
 require('./server/auth/auth')(app); // After app.use(sessions)
 
 // Server
 const server = new ApolloServer({
-    typeDefs: gql`${fs.readFileSync(__dirname.concat('/server/schema/schema.graphql'), 'utf8')}`,
+    typeDefs: gql `${fs.readFileSync(__dirname.concat('/server/schema/schema.graphql'), 'utf8')}`,
     resolvers: {
         Mutation,
         Query,
         Requisition,
         Stock,
     },
-    playground: true,
-    context: ({ req, res }) => {
-        return { req, res };
+    playground: !PRODUCTION,
+    context: ({
+        req,
+        res
+    }) => {
+        return {
+            req,
+            res
+        };
     },
-    debug: PRODUCTION
+    debug: !PRODUCTION
 });
 
-server.applyMiddleware({ app });
+server.applyMiddleware({
+    app
+});
 
 app.use((req, res) => {
     const regex = /\.\w+$/ /// Match all extensions
@@ -75,6 +98,17 @@ app.use((req, res) => {
         path.join(__dirname, 'public', 'index.html')
     );
 });
+
+// Add default user if user does not exist
+User.find({},(err, res) => {
+    if (res.length === 0) createDefaultUser({
+        name: 'admin',
+        role: 'admin',
+        username: 'admin',
+        password: 'admin'
+    })
+});
+
 
 const PORT = process.env.PORT || 8080;
 https.createServer({
